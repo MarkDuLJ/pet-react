b78d7282806b9592a8b05d64d04fb2b96efe97a1
Mark Du <dulongjiebackup@gmail.com>
2023-07-25 15:20:08 -0400

    framework code

---
+++ pet-react/src/main/java/com/spring/petreact/PetReactApplication.java
@@ -0,0 +1,13 @@
+package com.spring.petreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetReactApplication.class, args);
+	}
+
+}
